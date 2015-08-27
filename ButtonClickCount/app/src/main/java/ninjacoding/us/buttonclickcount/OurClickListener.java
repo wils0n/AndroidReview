ca631d624bcbe4ff0737580d5642abe68070983d
wils0n <wiljm0.0@gmail.com>
2015-08-27 16:32:01 -0500

    Project done!

--- ButtonClickCount/app/src/main/java/ninjacoding/us/buttonclickcount/OurClickListener.java
+++ ButtonClickCount/app/src/main/java/ninjacoding/us/buttonclickcount/OurClickListener.java
@@ -7,12 +7,32 @@ import android.view.View;
  */
 public class OurClickListener implements View.OnClickListener{
 
+    private int count;
     MainActivity caller;
+
     OurClickListener(MainActivity activity){
         this.caller = activity;
+        this.count = 0;
     }
 
     public void onClick(View view){
-        caller.theTextView.setText("The button got clicked");
+        count = count + 1;
+        String out = "The button has been clicked " + count + " time";
+
+        if (count != 1){
+            out += "s";
+        }
+        caller.theTextView.setText(out);
+
+        // Otra forma
+        
+        //String tempResult = Integer.toString(count);
+        //antiguamente se hacia lo anterior, ahora setText lo castea automaticamente
+
+        //if (count == 1){
+        //    caller.theTextView.setText("The button has been clicked " + count + " time.");
+        //} else {
+        //    caller.theTextView.setText("The button has been clicked " + count + " times.");
+        //}
     }
 }
